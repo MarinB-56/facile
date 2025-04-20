4b26b521be5164c2981d51620a2bce993dda795d
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-04-20 22:34:05 +0200

    Delete entity of destination.java. Useless table

--- src/main/java/org/acme/Destination.java
+++ src/main/java/org/acme/Destination.java
@@ -6,8 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "destinations")
+// @Entity
+//@Table(name = "destinations")
 public class Destination {
   // Attributs
   @Id
