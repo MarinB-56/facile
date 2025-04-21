20e9aa91015d200b357c84685d75ea68772e0d23
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-04-21 14:36:58 +0200

    Create and implement SubtripRepository

---
+++ src/main/java/org/acme/SubtripRepository.java
@@ -0,0 +1,44 @@
+package org.acme;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@ApplicationScoped
+public class SubtripRepository {
+  @PersistenceContext
+  EntityManager em;
+
+  public void save(Subtrip subtrip){
+    em.persist(subtrip);
+  }
+
+  public Subtrip findById(Long id){
+    return em.find(Subtrip.class, id);
+  }
+
+  public List<Subtrip> findAll(){
+    return em.createQuery("FROM Subtrip", Subtrip.class).getResultList();
+  }
+
+  public void delete(Subtrip subtrip){
+    if(em.contains(subtrip)){
+      em.remove(subtrip);
+    }else {
+      em.remove(em.merge(subtrip));
+    }
+  }
+
+  public void deleteById(Long id){
+    Subtrip subtrip = findById(id);
+    if(subtrip != null){
+      delete(subtrip);
+    }
+  }
+
+  public Subtrip update(Subtrip subtrip){
+    return em.merge(subtrip);
+  }
+}
