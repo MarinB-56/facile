c28e959b270b668ba21b1505e3d58381ebafe612
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-05-18 15:39:54 +0200

    Delete default style from components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TripConfiguratorComponent } from "./trip-configurator/trip-configurator.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TripConfiguratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
