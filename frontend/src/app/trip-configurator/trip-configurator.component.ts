2ae3eb171cdc7d934abc856acc89b5fd905cfcbd
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-05-18 18:11:42 +0200

    Start main page design

--- frontend/src/app/trip-configurator/trip-configurator.component.ts
+++ frontend/src/app/trip-configurator/trip-configurator.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TripDestinationsComponent } from "./trip-destinations/trip-destinations.component";
 import { TripDatesComponent } from "./trip-dates/trip-dates.component";
+import { TripSearchButtonComponent } from "./trip-search-button/trip-search-button.component";
 
 @Component({
   selector: 'app-trip-configurator',
-  imports: [TripDestinationsComponent, TripDatesComponent],
+  imports: [TripDestinationsComponent, TripDatesComponent, TripSearchButtonComponent],
   templateUrl: './trip-configurator.component.html',
   styleUrl: './trip-configurator.component.css'
 })
