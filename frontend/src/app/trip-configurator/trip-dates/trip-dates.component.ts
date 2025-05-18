1c38a4d00b3ee152b6a4cf7989cd5d0c654a475d
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-05-18 18:18:40 +0200

    Add dates-picker and destination-picker to their parents components

--- frontend/src/app/trip-configurator/trip-dates/trip-dates.component.ts
+++ frontend/src/app/trip-configurator/trip-dates/trip-dates.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DatePickerComponent } from "./date-picker/date-picker.component";
 
 @Component({
   selector: 'app-trip-dates',
-  imports: [],
+  imports: [DatePickerComponent],
   templateUrl: './trip-dates.component.html',
   styleUrl: './trip-dates.component.css'
 })
--- frontend/src/app/trip-configurator/trip-destinations/trip-destinations.component.ts
+++ frontend/src/app/trip-configurator/trip-destinations/trip-destinations.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DatePickerComponent } from "../trip-dates/date-picker/date-picker.component";
+import { DestinationPickerComponent } from "./destination-picker/destination-picker.component";
 
 @Component({
   selector: 'app-trip-destinations',
-  imports: [],
+  imports: [DatePickerComponent, DestinationPickerComponent],
   templateUrl: './trip-destinations.component.html',
   styleUrl: './trip-destinations.component.css'
 })
