c58e95510dd2d92dd6957fdf6642d14b9a96c9aa
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-05-20 18:29:15 +0200

    Update autocomplete catch to keep only 5 first results of the api response.
    Display the results in an ul list under the input item

--- frontend/src/app/trip-configurator/trip-destinations/destination-picker/destination-picker.component.ts
+++ frontend/src/app/trip-configurator/trip-destinations/destination-picker/destination-picker.component.ts
@@ -12,12 +12,14 @@ export class DestinationPickerComponent {
   private http = inject(HttpClient);
   private urlAutoComplete = "https://dictionary.lewagon.com/autocomplete/";
 
+  autoCompleteResults : String[] = [];
+
   @Input() placeholderText = "";
   @Input() valueText = "";
   @Input() isReadOnly :boolean = false; // Par défaut, input modifiable
   @Input() isDisabled :boolean = false;
 
-  autoComplete(event :Event){
+  autoComplete(event :Event) :void {
     const input = event.target as HTMLInputElement;
     const value = input.value;
     console.log(value);
@@ -25,7 +27,15 @@ export class DestinationPickerComponent {
     this.http
       .get<any>(`${this.urlAutoComplete}${value}`)
       .subscribe(response => {
-        console.log(response.words);
-      })
+        if(response.words != undefined) {
+          this.autoCompleteResults = response.words.slice(0, 5);
+        } else {
+          this.autoCompleteResults = [];
+        }
+      });
+  }
+
+  displayProposals(){
+    //Insérer les propositions dans le dom
   }
 }
