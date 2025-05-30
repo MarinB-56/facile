849bd427b6548ab2f826d059edbfd3bfaf209898
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-05-30 11:51:04 +0200

    Create asynchrone event to manage the user input.
    The api call is made only after 400ms without any input from the user. (to prevent useless api calls).
    The api call is not made if the input is less than 3 char long.
    The api call is not made if nothing has changed inside the input.
    If the api call is made but a new api call is made before the api has returned the answer, the api call is canceled and a new one is made.

--- frontend/src/app/trip-configurator/trip-destinations/destination-picker/destination-picker.component.ts
+++ frontend/src/app/trip-configurator/trip-destinations/destination-picker/destination-picker.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, Input, signal, HostListener, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, inject, Input, signal, HostListener, ElementRef, Output, EventEmitter, input } from '@angular/core';
 import { fakeAsync } from '@angular/core/testing';
 import { Destination } from '../../../models/destination.model';
+import { debounce, debounceTime, distinctUntilChanged, filter, Subject, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-destination-picker',
@@ -23,16 +24,27 @@ export class DestinationPickerComponent {
 
   @Output() selectedDestination = new EventEmitter<Destination>();
 
+  private searchTerms = new Subject<string>();
+  private searchSub: Subscription;
+
+  // Création d'un flux asynchrone
+  constructor(){
+    this.searchSub = this.searchTerms.pipe(
+      debounceTime(400), // On attend 400 ms sans input avant de passer à la suite.
+      filter((input: string) => input.length >= 3), // On filtre les input de moins de 3 char
+      distinctUntilChanged(), // On ne fait rien si rien n'a changé dans l'input
+      switchMap((input: string) => this.http.get<any>(`${this.urlAutoComplete}${encodeURIComponent(input)}`)) // Appel de l'url avec l'input
+    ).subscribe(response => {
+      this.displayProposals(response);
+    })
+  }
+
   autoComplete(event :Event) :void {
     const input = event.target as HTMLInputElement;
     this.valueText = input.value;
 
     if(this.valueText !== ''){
-      this.http
-        .get<any>(`${this.urlAutoComplete}${this.valueText}`)
-          .subscribe(response => {
-            this.displayProposals(response);
-          });
+      this.searchTerms.next(this.valueText);
     }else {
       this.autoCompleteResults.set([]);
       this.isDropdownOpen.set(false);
@@ -65,5 +77,9 @@ export class DestinationPickerComponent {
     console.log(proposal);
     this.valueText = proposal.name; // Affuchage du choix à l'utilisateur (update de l'input)
     this.selectedDestination.emit(proposal); // Transmettre la valeur sélectionnée au composant parent
+  }
+
+  ngOnDestroy(): void {
+    this.searchSub.unsubscribe();
   }
 }
