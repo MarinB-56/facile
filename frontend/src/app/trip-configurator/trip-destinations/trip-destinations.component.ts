3fc970dc3d6c9833b32a2ed4b587458c24688882
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-05-30 16:01:36 +0200

    Swap destination pickers when Swap button is used.
    User can now choose if belle ile is the destination or the origin of the trip

--- frontend/src/app/trip-configurator/trip-destinations/trip-destinations.component.ts
+++ frontend/src/app/trip-configurator/trip-destinations/trip-destinations.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { DatePickerComponent } from "../trip-dates/date-picker/date-picker.component";
 import { DestinationPickerComponent } from "./destination-picker/destination-picker.component";
 import { Destination } from '../../models/destination.model';
@@ -12,8 +12,16 @@ import { Destination } from '../../models/destination.model';
 export class TripDestinationsComponent {
   @Output() selectedDestination = new EventEmitter<Destination>();
 
+  // La ville choisie par l'utilisateur est l'origine du voyage
+  isTripOrigin = signal<boolean>(true);
+
   // Récupération de la destination choisie par l'utilisateur
   onDestinationSelected(destination: Destination) {
     this.selectedDestination.emit(destination); // Envoi de la destination choisie au composant parent
+  }
+
+  // Swap button
+  swapDestinations() {
+    this.isTripOrigin.set(!this.isTripOrigin());
   }
 }
