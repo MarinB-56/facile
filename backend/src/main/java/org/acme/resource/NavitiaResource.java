3069bb1d39e9bc1e92c36416c94347582723dcee
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-06-25 19:06:50 +0200

    Update NavitiaResource to expose a Post endpoint and allow the frontend to send Trip datas.

--- backend/src/main/java/org/acme/resource/NavitiaResource.java
+++ backend/src/main/java/org/acme/resource/NavitiaResource.java
@@ -1,9 +1,11 @@
 package org.acme.resource;
 
 import org.acme.client.NavitiaClient;
-import org.acme.dto.DestinationDTO;
+import org.acme.dto.TripDTO;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+// import jakarta.enterprise.inject.Produces; // Removed, not needed for REST endpoints
+import jakarta.ws.rs.Produces;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DefaultValue;
@@ -40,8 +42,14 @@ public class NavitiaResource {
   @POST
   @Path("/search")
   @Consumes(MediaType.APPLICATION_JSON)
-  public void searchTrip(DestinationDTO destination){
-    System.out.println(destination.id);
+  @Produces(MediaType.APPLICATION_JSON)
+  public void searchTrip(TripDTO trip){
+    if (trip == null) {
+      System.out.println("TripDTO is null");
+    } else {
+      // Utilisation des données
+      System.out.println(trip.toString());
+    }
   }
 }
 
