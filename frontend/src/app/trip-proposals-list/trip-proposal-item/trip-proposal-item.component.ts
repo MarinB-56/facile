e3e46851500dc8f5179a7c3cecddc0a75bfd5742
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-08-23 15:41:39 +0200

    Generate trip component item

---
+++ frontend/src/app/trip-proposals-list/trip-proposal-item/trip-proposal-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-proposal-item',
+  imports: [],
+  templateUrl: './trip-proposal-item.component.html',
+  styleUrl: './trip-proposal-item.component.css'
+})
+export class TripProposalItemComponent {
+
+}
