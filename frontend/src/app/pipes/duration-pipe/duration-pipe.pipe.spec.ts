7b3efa9a4dc8dbd7a095cc9cb1e2095e448883f2
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-09-11 16:49:22 +0200

    Write tests to test the duration pipe

---
+++ frontend/src/app/pipes/duration-pipe/duration-pipe.pipe.spec.ts
@@ -0,0 +1,34 @@
+import { DurationPipePipe } from './duration-pipe.pipe';
+
+describe('DurationPipePipe', () => {
+  const pipe = new DurationPipePipe();
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('Affiche 0h0 pour 0 secondes', () => {
+    expect(pipe.transform(0)).toBe('0h');
+  });
+
+  it('Affiche 1h pour 3600 secondes', () => {
+    expect(pipe.transform(3600)).toBe('1h');
+  });
+
+  it('Affiche 42 min pour 2542 secondes', () => {
+    expect(pipe.transform(2542)).toBe('42 min');
+  });
+
+  it('Affiche 0h59 pour 3540 secondes', () => {
+    expect(pipe.transform(3540)).toBe('0h59');
+  });
+
+  it('Affiche 2h30 pour 9000 secondes', () => {
+    expect(pipe.transform(9000)).toBe('2h30');
+  });
+
+  it('Affiche 0h0 pour 30 secondes', () => {
+    expect(pipe.transform(30)).toBe('0h');
+  });
+
+});
