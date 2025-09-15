ecb02680ff89bf97c4785915597cc3ff52937bd8
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-09-15 22:09:20 +0200

    Fix time pipe to pass all tests

--- frontend/src/app/pipes/time-pipe/time-pipe.pipe.ts
+++ frontend/src/app/pipes/time-pipe/time-pipe.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, ...args: unknown[]): string {
+    // On récupère value sous forme de 20250920T065300
+    let year = parseInt(value.slice(0,4));
+    let month = parseInt(value.slice(4,6));
+    let day = parseInt(value.slice(6,8));
+    let hour = parseInt(value.slice(9,11));
+    let minutes = parseInt(value.slice(11,13));
+    let seconds = parseInt(value.slice(13,15));
+
+    let date = new Date(year, month, day, hour, minutes, seconds);
+
+    return `${date.getHours()}h${date.getMinutes()}`;
   }
 
 }
