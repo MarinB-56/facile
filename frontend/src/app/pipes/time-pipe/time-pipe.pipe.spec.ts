ca28add9e5111df7416a379dfc349b20db357cae
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-09-15 22:09:05 +0200

    Create tests to check time pipe

--- frontend/src/app/pipes/time-pipe/time-pipe.pipe.spec.ts
+++ frontend/src/app/pipes/time-pipe/time-pipe.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { TimePipePipe } from './time-pipe.pipe';
 
 describe('TimePipePipe', () => {
+  const pipe = new TimePipePipe();
+
   it('create an instance', () => {
-    const pipe = new TimePipePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Affiche 6h53 pour la date 20250920T065300', () => {
+    expect(pipe.transform('20250920T065300')).toBe('6h53');
+  });
+
+  it('Affiche 8h55 pour la date 20250920T085500', () => {
+    expect(pipe.transform('20250920T085500')).toBe('8h55');
+  });
+
+  it('Affiche 12h58 pour la date 20250920T125800', () => {
+    expect(pipe.transform('20250920T125800')).toBe('12h58');
   });
 });
