8a4e793b9e6064c991dff9ba51a704d5991b58c1
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-09-25 20:20:01 +0200

    Add httpclient testing modules and create required inputs in the test

--- frontend/src/app/trip-configurator/trip-destinations-form/trip-destinations-form.component.spec.ts
+++ frontend/src/app/trip-configurator/trip-destinations-form/trip-destinations-form.component.spec.ts
@@ -1,19 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TripDestinationsFormComponent } from './trip-destinations-form.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { Destination } from '../../models/destination.model';
+// import { BELLE_ILE } from '../constants/destination.constants';
+import { BELLE_ILE } from '../../constants/destination.constants';
 
-describe('TripDestinationsComponent', () => {
+describe('TripDestinationsFormComponent', () => {
   let component: TripDestinationsFormComponent;
   let fixture: ComponentFixture<TripDestinationsFormComponent>;
 
+  const departure: Destination = {name: "Paris", embedded_type: "stop_area", id: "XXX"}
+  const arrival: Destination = {name: "Auray (Auray)", embedded_type: "stop_area", id: "stop_area:SNCF:87476200"}
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TripDestinationsFormComponent]
+      imports: [TripDestinationsFormComponent],
+      providers: [
+        provideHttpClient(), // Fournit HttpClient
+        provideHttpClientTesting(), // Fournit les outils de test pour HttpClient
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(TripDestinationsFormComponent);
     component = fixture.componentInstance;
+
+    // Ajout des input required
+    // departure = input.required<Destination>();
+    // arrival = input.required<Destination>();
+    fixture.componentRef.setInput('arrival', arrival);
+    fixture.componentRef.setInput('departure', departure);
+
+
     fixture.detectChanges();
   });
 
