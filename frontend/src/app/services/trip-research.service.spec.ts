b3a49dd1c48c9cc5e7ce621636040bd16bd552d3
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-10-02 13:11:30 +0200

    Create tests for trip service to improve tests coverage

--- frontend/src/app/services/trip-research.service.spec.ts
+++ frontend/src/app/services/trip-research.service.spec.ts
@@ -13,4 +13,30 @@ describe('TripResearchService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // On veut que setResults(data) fasse que this.results = data
+  // Donc this.results doit être égal à data
+  it('setResults should set results successfully', () => {
+    // Création data
+    const testData = {id: 1, name: 'Test trip'};
+    service.setResults(testData);
+
+    expect((service as any).results).toEqual(testData);
+  })
+
+  it('getResults should get results data successfully', () => {
+    const testData = {id: 1, name: 'Test trip'};
+
+    (service as any).results = testData;
+
+    expect(service.getResults()).toEqual(testData);
+  })
+
+  it('clearData should clear results successfully and be equal to null', () => {
+    const testData = {id: 1, name: 'Test trip'};
+    (service as any).results = testData;
+    service.clearResults();
+
+    expect((service as any).results).toEqual(null); 
+  })
 });
