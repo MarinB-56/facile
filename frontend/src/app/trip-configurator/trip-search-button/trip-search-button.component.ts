ccaa93922dc761cd02745a2001178ff40dc44a22
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-10-05 10:41:35 +0200

    Update @Input to modern input()

--- frontend/src/app/trip-configurator/trip-search-button/trip-search-button.component.ts
+++ frontend/src/app/trip-configurator/trip-search-button/trip-search-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-trip-search-button',
@@ -7,7 +7,8 @@ import { Component, Input } from '@angular/core';
   styleUrl: './trip-search-button.component.scss'
 })
 export class TripSearchButtonComponent {
-  @Input() isDisabled: boolean = true;
+  // @Input() isDisabled: boolean = true;
+  isDisabled = input<boolean>(true);
 
   onSearch(){
     console.log("On lance la recherche");
