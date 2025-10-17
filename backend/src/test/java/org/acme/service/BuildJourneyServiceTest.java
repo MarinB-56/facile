0b8ef279fa0e59b331cec8e7fd7dfde66d6236a3
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-10-17 12:19:11 +0200

    Create test to test JourneyDTO special functions
    Function to get first departure date time and last arrial date time

---
+++ backend/src/test/java/org/acme/service/BuildJourneyServiceTest.java
@@ -0,0 +1,17 @@
+package org.acme.service;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class BuildJourneyServiceTest {
+
+  @Test
+  void shouldReturnTrueIfBoatIsFirstSection(){
+
+    // Je récupère un TripDTO
+    // Je lance la fonction get
+
+  }
+}
