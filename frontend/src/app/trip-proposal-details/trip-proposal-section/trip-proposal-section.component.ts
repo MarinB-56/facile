b6267d23f38722049e67ecfbd1a2aa5da11963c4
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-10-19 16:10:58 +0200

    Modify templates to include trains, boat and buses companies and details

--- frontend/src/app/trip-proposal-details/trip-proposal-section/trip-proposal-section.component.ts
+++ frontend/src/app/trip-proposal-details/trip-proposal-section/trip-proposal-section.component.ts
@@ -3,10 +3,11 @@ import { Trip } from '../../models/trip.model';
 import { Section } from '../../models/section.model';
 import { TimePipePipe } from "../../pipes/time-pipe/time-pipe.pipe";
 import { DurationPipePipe } from "../../pipes/duration-pipe/duration-pipe.pipe";
+import { TransportTypePipePipe } from '../../pipes/transport-type-pipe/transport-type-pipe.pipe';
 
 @Component({
   selector: 'app-trip-proposal-section',
-  imports: [TimePipePipe, DurationPipePipe],
+  imports: [TimePipePipe, DurationPipePipe, TransportTypePipePipe],
   templateUrl: './trip-proposal-section.component.html',
   styleUrl: './trip-proposal-section.component.scss'
 })
