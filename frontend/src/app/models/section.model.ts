f152bf1e9a1913792f0d962a9128699d34c83dd6
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-10-19 15:21:17 +0200

    Add display informations to section model, so that these informations are available in angular's templates

--- frontend/src/app/models/section.model.ts
+++ frontend/src/app/models/section.model.ts
@@ -5,8 +5,14 @@ export interface Section{
   departure_date_time: String,
   duration: number,
   from: Destination,
-  sectionDuration: number,
+  section_duration: number,
   to: Destination,
   type: String,
   transport_type: String
+  display_informations: {
+    commercial_mode: String,
+    company: String,
+    network: String,
+    physical_mode: String
+  }
 }
