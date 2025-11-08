cde1daa27fc7263416738fc2d73f1a93aab60483
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-11-08 15:16:22 +0100

    Make properties not compulsory when desierialising a JourneyDTO

--- backend/src/main/java/org/acme/dto/JourneyDTO.java
+++ backend/src/main/java/org/acme/dto/JourneyDTO.java
@@ -5,8 +5,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JourneyDTO {
   @JsonProperty("duration") // Précise le nom tel qu'il est dans le json
   private int totalDuration;
