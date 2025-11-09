5f55d26b3e218316300f7268803b8f918b355699
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-11-09 16:35:54 +0100

    Add header everywhere in the app
    User can go back to home page by clicking on the title

--- frontend/src/app/trip-proposal-details/trip-proposal-details.component.ts
+++ frontend/src/app/trip-proposal-details/trip-proposal-details.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Journey } from '../models/journey.model';
 import { DurationPipePipe } from '../pipes/duration-pipe/duration-pipe.pipe';
-import { TimePipePipe } from '../pipes/time-pipe/time-pipe.pipe';
 import { ConnectionPipePipe } from '../pipes/connection-pipe/connection-pipe.pipe';
 import { TripProposalSectionComponent } from "./trip-proposal-section/trip-proposal-section.component";
+import { TripHeaderComponentComponent } from '../trip-header-component/trip-header-component.component';
 
 @Component({
   selector: 'app-trip-proposal-details',
-  imports: [DurationPipePipe, TimePipePipe, ConnectionPipePipe, TripProposalSectionComponent],
+  imports: [
+    DurationPipePipe,
+    ConnectionPipePipe,
+    TripProposalSectionComponent,
+    TripHeaderComponentComponent
+  ],
   templateUrl: './trip-proposal-details.component.html',
   styleUrl: './trip-proposal-details.component.scss'
 })
