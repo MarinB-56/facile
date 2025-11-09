0b3f4179a00a8bf6cce08d08b8374d7a6e85a8b4
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-11-09 14:51:45 +0100

    Modify shared service to save searching trip details
    Search details can now be printed on all pages (dates or destinations)

--- frontend/src/app/services/trip-research.service.ts
+++ frontend/src/app/services/trip-research.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Trip } from '../models/trip.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripResearchService {
   private results: any;
+  private search: any;
 
   constructor() {
 
+  }
+
+  setSearch(data: Trip){
+    this.search = data;
+  }
+
+  getSearch() :Trip {
+    return this.search;
   }
 
   setResults(data: any) {
