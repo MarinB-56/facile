c1c100a0bdd6b5226d9daaf3fbc7c521eab10310
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-11-11 19:11:49 +0100

    Create a new endpoint to expose boat schedule

--- backend/src/main/java/org/acme/resource/GtfsResource.java
+++ backend/src/main/java/org/acme/resource/GtfsResource.java
@@ -1,32 +1,46 @@
 package org.acme.resource;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.acme.service.GtfsService;
-import org.onebusaway.gtfs.model.Route;
-import org.onebusaway.gtfs.model.Stop;
-import org.onebusaway.gtfs.model.StopTime;
-import org.onebusaway.gtfs.model.Trip;
 
-import java.time.LocalDate;
+import org.acme.dto.SectionDTO;
+import org.acme.dto.TripDTO;
+import org.acme.service.GtfsService;
+import org.onebusaway.gtfs.model.StopTime;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Path("/gtfs")
 public class GtfsResource {
   @Inject
   GtfsService gtfsService;
 
-  // @GET
-  // @Path("/essai")
-  // public Map<String, List<StopTime>> demarrageGTFS(){
-  //   return gtfsService.getTripsFromTo("I56QUI", "I56BIP");
-  // }
+  @POST
+  @Path("/schedule")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Set<SectionDTO> demarrageGTFS(TripDTO trip){
+    System.out.println("Arrivée dans l'endpoint gtfs");
+    ZoneId parisZone = ZoneId.of("Europe/Paris");
+
+    // Convertir LocalDateTime reçu comme s'il était UTC en LocalDateTime Paris
+    OffsetDateTime odtUtc = trip.getDate().atOffset(ZoneOffset.UTC);
+    LocalDateTime localParis = odtUtc.atZoneSameInstant(parisZone).toLocalDateTime();
+
+    trip.setDate(localParis);
+
+    return gtfsService.getSectionsFromGtfsData(trip);
+  }
 
   @GET
   @Path("/stop")
