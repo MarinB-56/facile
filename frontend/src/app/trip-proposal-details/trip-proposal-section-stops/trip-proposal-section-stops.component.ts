9c3dc7292082b7e1f628084e5b1a850364f2cecb
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-11-27 16:23:25 +0100

    Refactor stop details on itinary proposal details page

--- frontend/src/app/trip-proposal-details/trip-proposal-section-stops/trip-proposal-section-stops.component.ts
+++ frontend/src/app/trip-proposal-details/trip-proposal-section-stops/trip-proposal-section-stops.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, output, Signal, signal, WritableSignal } from '@angular/core';
+import { MatAccordion, MatExpansionPanel, MatExpansionModule } from "@angular/material/expansion";
+import { TimePipePipe } from "../../pipes/time-pipe/time-pipe.pipe";
 
 @Component({
   selector: 'app-trip-proposal-section-stops',
-  imports: [],
+  imports: [MatAccordion, MatExpansionPanel, MatExpansionModule, TimePipePipe],
   templateUrl: './trip-proposal-section-stops.component.html',
   styleUrl: './trip-proposal-section-stops.component.scss'
 })
 export class TripProposalSectionStopsComponent {
+  section = input<any>();
 
+  icon = input<string>();
+  title = input<string>();
+  
+  panelOpenState = input();
+  panelOpenChange = output<boolean>();
 }
+
+// icon = input.required<string>();
+// title = input.required<string>();
--- frontend/src/app/trip-proposal-details/trip-proposal-section/trip-proposal-section.component.ts
+++ frontend/src/app/trip-proposal-details/trip-proposal-section/trip-proposal-section.component.ts
@@ -4,15 +4,39 @@ import { DurationPipePipe } from "../../pipes/duration-pipe/duration-pipe.pipe";
 import { TransportTypePipePipe } from '../../pipes/transport-type-pipe/transport-type-pipe.pipe';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule, MatExpansionPanel } from '@angular/material/expansion';
+import { TripProposalSectionStopsComponent } from "../trip-proposal-section-stops/trip-proposal-section-stops.component";
 
 @Component({
   selector: 'app-trip-proposal-section',
   standalone: true,
-  imports: [TimePipePipe, DurationPipePipe, TransportTypePipePipe, CommonModule, MatExpansionModule],
+  imports: [TimePipePipe, DurationPipePipe, TransportTypePipePipe, CommonModule, MatExpansionModule, TripProposalSectionStopsComponent],
   templateUrl: './trip-proposal-section.component.html',
   styleUrl: './trip-proposal-section.component.scss'
 })
 export class TripProposalSectionComponent {
   section = input.required<any>();
   readonly panelOpenState = signal(false);
+
+  getIcon() : string {
+    if( this.section().display_informations.physical_mode.includes("Train") || this.section().display_informations.physical_mode.includes("TER") ) {
+      return "directions_railway_2";
+    }else if(this.section().display_informations.physical_mode.includes("Autocar")){
+      return "directions_bus"
+    }else if(this.section().display_informations.physical_mode.includes("Bateau")){
+      return "directions_boat";
+    }
+
+    return "question_mark";
+  }
+
+  getTransportTitle(): string {
+    if (this.section().display_informations.physical_mode.includes("Train")) {
+      return this.section().display_informations.network;
+    } else if (this.section().display_informations.physical_mode.includes("TER")) {
+      return this.section().display_informations.physical_mode;
+    } else if (this.section().display_informations.physical_mode.includes("Bateau")) {
+      return `${this.section().display_informations.physical_mode} - ${this.section().display_informations.network}`;
+    }
+    return this.section().display_informations.physical_mode;
+  }
 }
