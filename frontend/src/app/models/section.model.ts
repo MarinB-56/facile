b7db56be93c37c4ce0367ef149499dd9b69ee755
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-12-28 16:21:37 +0100

    Add conditions to show expansion arrow.
    If nos stops during the section, arrow is hidden and expansion panel is disabled.
    If stops during the section, nothing changes

--- frontend/src/app/models/section.model.ts
+++ frontend/src/app/models/section.model.ts
@@ -3,16 +3,25 @@ import { Destination } from "./destination.model";
 export interface Section{
   arrival_date_time: String,
   departure_date_time: String,
-  duration: number,
-  from: Destination,
-  section_duration: number,
-  to: Destination,
-  type: String,
-  transport_type: String,
   display_informations: {
     commercial_mode: String,
     company: String,
     network: String,
     physical_mode: String
   }
+  duration: number,
+  from: Destination,
+  section_duration: number,
+  // stop_date_times: [
+  //   arrival_date_time: String,
+  //   departure_date_time: String,
+  //   stop_point: [
+  //     id: String,
+  //     embedded_type: String,
+  //     name: String
+  //   ]
+  // ],
+  to: Destination,
+  type: String,
+  transport_type: String,
 }
--- frontend/src/app/trip-proposal-details/trip-proposal-section-stops/trip-proposal-section-stops.component.ts
+++ frontend/src/app/trip-proposal-details/trip-proposal-section-stops/trip-proposal-section-stops.component.ts
@@ -1,6 +1,7 @@
 import { Component, input, output, Signal, signal, WritableSignal } from '@angular/core';
 import { MatAccordion, MatExpansionPanel, MatExpansionModule } from "@angular/material/expansion";
 import { TimePipePipe } from "../../pipes/time-pipe/time-pipe.pipe";
+import { Section } from '../../models/section.model';
 
 @Component({
   selector: 'app-trip-proposal-section-stops',
@@ -13,9 +14,17 @@ export class TripProposalSectionStopsComponent {
 
   icon = input<string>();
   title = input<string>();
-  
+
   panelOpenState = input();
   panelOpenChange = output<boolean>();
+
+  hasStops(): boolean {
+    if(this.section()?.stop_date_times.length === 0){
+      return false;
+    }else{
+      return true;
+    }
+  }
 }
 
 // icon = input.required<string>();
